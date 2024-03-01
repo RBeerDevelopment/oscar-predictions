90b932f9a62ff254a199cee088ef50b138006f55
Robin Beer <contact@robin-beer.com>
2024-03-01 21:59:15 +0100

    add middleware rule to only allow admin user to access admin page

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,13 @@ import { NextResponse } from "next/server";
 export default authMiddleware({
   publicRoutes: ["/", "/image", "/image/iphone.png", "/opengraph-image"],
   afterAuth(auth, req, evt) {
+    if (
+      req.nextUrl.pathname === "/admin" &&
+      auth.userId !== process.env.ADMIN_USER_ID
+    ) {
+      const voteUrl = new URL("/dashboard", req.url);
+      return NextResponse.redirect(voteUrl);
+    }
     // Handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
